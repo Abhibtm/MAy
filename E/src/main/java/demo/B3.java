b381ab20ae637dcda90c00275c052fb330913314
ani-ruddha <aniruddhadas919@gmail.com>
2023-05-03 23:13:19 +0530

    GCSE6Thir

--- E/src/main/java/demo/B3.java
+++ E/src/main/java/demo/B3.java
@@ -3,6 +3,7 @@ package demo;
 public class B3 {
 	public static void main(String[] args) {
 		System.out.println("Hii");
+		System.out.println("Next");
 		System.out.println("Hello");
 		System.out.println("Tik");
 	}
